cafc14d80d3ecccf4c1e2337c5c2e7ad8d46be42
Sarah Peterson <sarahp@nurelm.com>
2019-09-14 19:39:54 -0400

    updates about me copy, gif, social links

--- potted/src/App.js
+++ potted/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 
-// need copy
-// fix txt verify
-// undo menu highlight after scroll
-// add font awesome icons
 // add content for error pages
+// add router for instagram and quiz pages
+// make this page into components
+// gif - fix pot moving
+// fix form
 
 function App() {
   return (
@@ -45,7 +45,8 @@ function App() {
             <div className="about-text-p">
               <p>about</p>
             </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat varius dictum. Etiam eget consectetur ligula, id consectetur mi. Cras nibh libero, hendrerit id mollis dictum, fringilla in purus. Etiam commodo tincidunt porttitor.</p>
+            <p>Potted, PGH was born out of a shared passion of its two founders. In the process of filling their own homes with houseplants, they garnered a lot of knowledge about what plants can work best in certain situations. Now, they want to share their joy and knowledge of houseplants with you!</p>
+            <p>Indoor plants look great but they can also improve the air quality in your home. Some people say that they can even improve your physical and mental health. It can seem daunting to take on living things in your home and curate your unique plant collection, but, it doesn't have to be! Let the Potted team help you find a plant that fits your needs, whether that be pet safe, low light tolerant, low maintenance, and more.</p>
           </div>
           <div className="woman" />
         </div>
@@ -54,26 +55,35 @@ function App() {
             <div className="contact-text-p">
               <p>contact us</p>
             </div>
-            <p>Interested in your own specialized interior plant design? shoot us an email, instagram dm or fill out the  form below to get started today!</p>
+            <p className="interested">Interested in your own specialized interior plant design? shoot us an email, instagram dm or fill out the  form below to get started today!</p>
           </div>
-          <form className="form" id="contact">
+          <form netlify className="form" id="contact" action="/thanks.html">
+            <img src="NewPotGif.gif" alt="houseplant" className="houseplant" />
             <div className="form-div">
               <p className="form-text">
                 Name:
               </p>
-              <input type="text" name="email" />
+              <input className="contact-input" type="text" name="email" />
             </div>
             <div className="form-div">
               <p className="form-text">
                 Email:
               </p>
-              <input type="text" name="email" />
+              <input className="contact-input" type="text" name="email" />
             </div>
             <div className="form-div help">
               <p className="form-text">
                 What Can We Help You With?:
               </p>
-              <input type="text" name="lastname" />
+              <textarea className="contact-input" rows="10" cols="30" />
+            </div>
+            <div className="icons">
+              <a href="mailto: pottedpgh@gmail.com" className="icon">
+                <i className="far fa-envelope fa-3x"></i>
+              </a>
+              <a href="https://www.instagram.com/pottedpgh/" rel="noopener noreferrer" target="_blank" className="icon">
+                <i className="fab fa-instagram fa-3x"></i>
+              </a>
             </div>
             <button className="submit-button" type="submit" form="form1" value="Submit">
               Submit
