97fdd7e52ec37e9d32efb4fe67e687cccadef7da
Sarah Peterson <sarahp@nurelm.com>
2019-09-15 10:19:31 -0400

    adds insta placeholder back

---
+++ potted/src/pages/Instagram/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './insta.css';
+
+class Home extends Component {
+	render() {
+		return (
+			<div className="insta-container">
+	      <div className="container">
+	        <p className="follow">
+	          Follow us on Instagram @pottedpgh
+	        </p>
+	      </div>
+			</div>
+    )
+  }
+}
+export default Home;
