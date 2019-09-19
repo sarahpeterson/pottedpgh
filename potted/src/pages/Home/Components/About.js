d8e56a412278cc0a53bd7b1ee3812a12e3e4a622
Sarah Peterson <sarahp@nurelm.com>
2019-09-19 18:43:36 -0400

    updates about section

--- potted/src/pages/Home/Components/About.js
+++ potted/src/pages/Home/Components/About.js
@@ -7,8 +7,21 @@ const About = () => (
       <div className="about-text-p">
         <p>about</p>
       </div>
-      <p>Potted, PGH was born out of a shared passion of its two founders. In the process of filling their own homes with houseplants, they garnered a lot of knowledge about what plants can work best in certain situations. Now, they want to share their joy and knowledge of houseplants with you!</p>
-      <p>Indoor plants look great but they can also improve the air quality in your home. Some people say that they can even improve your physical and mental health. It can seem daunting to take on living things in your home and curate your unique plant collection, but, it doesn't have to be! Let the Potted team help you find a plant that fits your needs, whether that be pet safe, low light tolerant, low maintenance, and more.</p>
+      <p>
+        Potted, PGH’s curated interior plant design strives to enhance your day-to-day living by
+        breathing new life into your space.
+      </p>
+      <p>
+        Indoor plants look great but they can also improve the air quality in your home. Some people say
+        that they can even improve your physical and mental health. It can seem daunting to take on
+        living things in your home, but it doesn&#39;t have to be! Let the Potted team help you find plants that
+        fit your needs, whether that be pet safe, low light tolerant, low maintenance, and more.
+      </p>
+      <p>
+        Potted, PGH was born out of a shared passion of its two founders. In the process of filling their
+        own homes with houseplants, they garnered a lot of knowledge about what plants can work best
+        in certain situations. Now, they want to share their joy and knowledge of houseplants with you!
+      </p>
     </div>
     <div className="woman" />
   </div>
