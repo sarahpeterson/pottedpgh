4ee47e877d6d5d5958e7cd910662764dc741c659
Sarah Peterson <sarahp@nurelm.com>
2020-04-12 12:06:39 -0400

    fixes id for netlify form

--- potted/src/pages/Home/Components/Form.js
+++ potted/src/pages/Home/Components/Form.js
@@ -2,38 +2,41 @@ import React from 'react';
 import '../home.css';
 
 const Form = () => (
-  <form netlify className="form" id="contact" action="/thanks.html">
-    <img src="NewPotGif.gif" alt="houseplant" className="houseplant" />
-    <div className="form-div">
-      <p className="form-text">
-        Name:
-      </p>
-      <input className="contact-input" type="text" name="email" />
-    </div>
-    <div className="form-div">
-      <p className="form-text">
-        Email:
-      </p>
-      <input className="contact-input" type="text" name="email" />
-    </div>
-    <div className="form-div help">
-      <p className="form-text">
-        What Can We Help You With?:
-      </p>
-      <textarea className="contact-input" rows="10" cols="30" />
-    </div>
-    <div className="icons">
-      <a href="mailto: pottedpgh@gmail.com" className="icon">
-        <i className="far fa-envelope fa-3x"></i>
-      </a>
-      <a href="https://www.instagram.com/pottedpgh/" rel="noopener noreferrer" target="_blank" className="icon">
-        <i className="fab fa-instagram fa-3x"></i>
-      </a>
-    </div>
-    <button className="submit-button" type="submit" form="form1" value="Submit">
-      Submit
-    </button>
-  </form>
+  <div id="form">
+    <form name="contact" className="form" action="/thanks.html" netlify-honeypot="bot-field" method="POST" netlify="true">
+      <img src="NewPotGif.gif" alt="houseplant" className="houseplant" />
+      <div className="form-div">
+        <p className="form-text">
+          Name:
+        </p>
+        <input className="contact-input" type="text" name="name" />
+      </div>
+      <div className="form-div">
+        <p className="form-text">
+          Email:
+        </p>
+        <input className="contact-input" type="text" name="email" />
+      </div>
+      <div className="form-div help">
+        <p className="form-text">
+          What Can We Help You With?:
+        </p>
+        <textarea className="contact-input" rows="10" cols="30" name="message" />
+      </div>
+      <div className="icons">
+        <a href="mailto: admin@pottedpgh.com" className="icon">
+          <i className="far fa-envelope fa-3x"></i>
+        </a>
+        <a href="https://www.instagram.com/pottedpgh/" rel="noopener noreferrer" target="_blank" className="icon">
+          <i className="fab fa-instagram fa-3x"></i>
+        </a>
+        <a href="/thank-you" rel="noopener noreferrer" className="icon">
+          <i className="fab fa-instagram fa-3x"></i>
+        </a>
+      </div>
+      <input className="submit-button" type="submit" name="submit" id="name" />
+    </form>
+  </div>
 );
 
 export default Form;
